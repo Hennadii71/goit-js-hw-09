5b83094f82bda59c055fecd8a674e0cb5e4643e2
Hennadii71 <genadisema@gmail.com>
2023-11-14 23:07:00 +0200

    Update task 1 change js

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -4,4 +4,27 @@ function getRandomHexColor() {
     .padStart(6, 0)}`;
 }
 
+const refs = {
+  buttonStart: document.querySelector('button[data-start]'),
+  buttonStop: document.querySelector('button[data-stop]'),
+  bodyColor: document.querySelector('body'),
+};
 
+refs.buttonStart.addEventListener('click', onChangeColorBody);
+refs.buttonStop.addEventListener('click', ofChangeColorBody);
+
+let timerId;
+
+function onChangeColorBody() {
+  refs.buttonStart.disabled = true;
+  refs.buttonStop.disabled = false;
+  timerId = setInterval(() => {
+    refs.bodyColor.style.backgroundColor = getRandomHexColor();
+  }, 1000);
+}
+
+function ofChangeColorBody() {
+  refs.buttonStart.disabled = false;
+  refs.buttonStop.disabled = true;
+  clearInterval(timerId);
+}
