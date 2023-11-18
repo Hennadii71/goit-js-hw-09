e02d92b3e38cbb924ac0ed193883ba66707fb409
Hennadii71 <genadisema@gmail.com>
2023-11-18 23:07:50 +0200

    Update timer.js add refs

--- src/js/02-timer.js
+++ src/js/02-timer.js
@@ -1,21 +1,23 @@
-import flatpickr from "flatpickr";
-import "flatpickr/dist/flatpickr.min.css";
+import flatpickr from 'flatpickr';
+import 'flatpickr/dist/flatpickr.min.css';
 import { Report } from 'notiflix/build/notiflix-report-aio';
 
 const options = {
-    enableTime: true,
-    time_24hr: true,
-    defaultDate: new Date(),
-    minuteIncrement: 1,
-    onClose(selectedDates) {
-      console.log(selectedDates[0]);
-    },
-  };
+  enableTime: true,
+  time_24hr: true,
+  defaultDate: new Date(),
+  minuteIncrement: 1,
+  onClose(selectedDates) {
+    console.log(selectedDates[0]);
+  },
+};
 
-  flatpickr("#datetime-picker", options);
+flatpickr('#datetime-picker', options);
 
-
-
-  
-
- 
+const refs = {
+  buttonStart: document.querySelector('[data-start]'),
+  counterDays: document.querySelector('[data-days'),
+  counterHours: document.querySelector('[data-hours]'),
+  counterMinutes: document.querySelector('[data-minutes]'),
+  counterSeconds: document.querySelector('[data-seconds]'),
+};
